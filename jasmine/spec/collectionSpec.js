8b593905053878e7b2aa6a6b3acb807edf560040
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2012-04-24 00:27:52 +0200

    add collection tests

--- jasmine/spec/collectionSpec.js
+++ jasmine/spec/collectionSpec.js
@@ -1,4 +1,16 @@
 describe("COLLECTIONS", function() {
-	it("should test something - not yet ;)", function() {
-	});   
+	
+	describe("Marker Collection", function() {
+		
+	  beforeEach(function() {
+		this.markercollection = new MarkerCollection;
+		this.marker = new Marker;
+	  });
+		  
+	  it("should add a model", function() {
+	    this.markercollection.add([this.marker]);
+	    expect(this.markercollection.length).toEqual(1);
+	  });		
+	});
+
 });

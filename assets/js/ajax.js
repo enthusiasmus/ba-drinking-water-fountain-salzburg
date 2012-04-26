049cc7e812ad8137bdf4d62245a6a1cc4e11d5d5
Nicole Buchegger <nbuchegger.mmt-b2010@fh-salzburg.ac.at>
2012-04-26 18:26:55 +0200

    fix problems with navigation

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -13,7 +13,9 @@ $.ajax({
 
 function getFeed() {
 	$('#map_canvas').hide();
-	$.get('/qpt2b/trinkbrunnen_salzburg_webapp/rss.php', {
+	$('#feed').show();
+
+	$.get('rss.php', {
 	  feed_url:'http://www.seppeisl.at/modules/news/rss.php?page_id=1',
 	}, function(xml) {
 		 getFeedItems(xml);

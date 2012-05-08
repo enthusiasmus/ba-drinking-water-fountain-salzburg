6fc45880a732ca2603fb97a54f3ad7146a355695
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2012-05-09 01:57:08 +0200

    added route request for the next fontain

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,16 +1,12 @@
 var AppRouter = Backbone.Router.extend({
   routes: {
   	"adress": "showAdressSearch",
-    "next": "showRouteNextSpring",
     "feed": "showRssFeed",
     "about": "showAbout",
     "*actions": "defaultRoute"
   },
   showAdressSearch: function(){
   	//alert("Adresseingabe zum Suchen")
-  },
-  showRouteNextSpring: function(){
-  	//alert("Route zum nächsten Trinkbrunnen")
   },
   showRssFeed: function(){
   	getFeed();

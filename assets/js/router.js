c19a0dced5ffcaa0b975f7958f6eacf93eef84b2
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2012-05-09 16:19:40 +0200

    change visibility of views in router

--- assets/js/router.js
+++ assets/js/router.js
@@ -9,20 +9,24 @@ var AppRouter = Backbone.Router.extend({
   index: function() {
   	$('#feed').hide();
   	$('#info').hide();
+  	$('#search').hide();
   },
   showAdressSearch: function(){
   	$('#feed').hide();
   	$('#info').hide();
+  	$('#map_canvas').show();  	
   	adressView.switchVisibility();
   },
   showRssFeed: function(){
   	getFeed();
   	$('#map_canvas').hide();
   	$('#info').hide();
+  	$('#search').hide();  	
   },
   showAbout: function(){
   	$('#map_canvas').hide();
   	$('#feed').hide();  	
+  	$('#search').hide();  	
   	$('#info').show();
   },
   defaultRoute: function( action ){

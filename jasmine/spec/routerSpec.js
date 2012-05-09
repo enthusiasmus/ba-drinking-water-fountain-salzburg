64bf65f27186c27a6bc6068099b007ab46e231d1
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2012-05-09 10:35:47 +0200

    test new jasmin-sinon framework

--- jasmine/spec/routerSpec.js
+++ jasmine/spec/routerSpec.js
@@ -1,4 +1,17 @@
-describe("ROUTERS", function() {
-	it("should test something - not yet ;)", function() {
-	});    
+describe("ROUTES", function() {
+  beforeEach(function() {
+    this.router = new AppRouter;
+    this.routeSpy = sinon.spy();
+    try {
+      Backbone.history.start({silent:true, pushState:true});
+    } catch(e) {}
+    this.router.navigate("elsewhere");
+  });
+  
+  it("fires the index route with a blank hash", function() {
+    this.router.bind("route:index", this.routeSpy);
+    this.router.navigate("", true);
+    expect(this.routeSpy).toHaveBeenCalledOnce();
+    expect(this.routeSpy).toHaveBeenCalledWith();
+  });
 });

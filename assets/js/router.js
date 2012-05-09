cc972dc3711ab65fa925a9e8024fca4ee34fa978
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2012-05-09 10:55:38 +0200

    add route and change specrunner

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,9 +1,13 @@
 var AppRouter = Backbone.Router.extend({
   routes: {
+  	"": "index",
   	"adress": "showAdressSearch",
     "feed": "showRssFeed",
     "about": "showAbout",
     "*actions": "defaultRoute"
+  },
+  index: function(){
+  	//index-action
   },
   showAdressSearch: function(){
   	//alert("Adresseingabe zum Suchen")

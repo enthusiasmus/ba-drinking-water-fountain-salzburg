ef9b4c4c6bbf2688c65694f010fa6d8c280bf6f4
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2012-05-14 09:35:20 +0200

    join router.js with main.js

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,23 +1,5 @@
-$.ajax({
-	async: true,
-	dataType: "json",
-	timeoutNumber: 5000,
-	url: "db/elements.php",
-	success: function(data){	
-		placeMarkers(data);
-	},
-	error: function(data){
-		alert("Die Trinkbrunnen konnten nicht geladen werden!");
-	}
-});
+
 
 function getFeed() {
-	$('#map_canvas').hide();
-	$('#feed').show();
 
-	$.get('rss.php', {
-	  feed_url:'http://www.seppeisl.at/modules/news/rss2.php?page_id=1&group_id=7',
-	}, function(xml) {
-		 getFeedItems(xml);
-	});
 }
--- assets/js/router.js
+++ assets/js/router.js
@@ -8,14 +8,48 @@ var AppRouter = Backbone.Router.extend({
     "*actions": "defaultRoute"
   },
   index: function() {
+  	$.ajax({
+			async: true,
+			dataType: "json",
+			timeoutNumber: 5000,
+			url: "db/elements.php",
+			success: function(data){
+		  	for(idx in data){
+					var markerModel = new MarkerModel({
+						latitude: data[idx].latitude, 
+						longitude: data[idx].longitude,
+						title: data[idx].f_key + ": " + data[idx].water_distributor + " - " + data[idx].fontain_name
+					});
+					markerCollection.push(markerModel, []);
+				}
+				mapView.addMarkerCollection(markerCollection);
+				mapView.placeMarkersToMap();
+			},
+			error: function(data){
+				alert("Die Trinkbrunnen konnten nicht geladen werden!");
+			}
+		});
 		this.displayOnly("map_canvas");	
   },
   showAdressSearch: function(){
   	adressView.switchVisibility();
 		this.displayOnly("map_canvas");	
   },
-  showRssFeed: function(){
-  	getFeed();
+  showRssFeed: function(){	
+		$.get('rss.php', {
+		  feed_url:'http://www.seppeisl.at/modules/news/rss2.php?page_id=1&group_id=7',
+		}, function(xml){
+			$(xml).find('item').each(function(){
+				var feedItemModel = new FeedItemModel({
+					title: $(this).find('title').text(), 
+					link: $(this).find('link').text(),
+					pubDate: $.format.date($(this).find('pubDate').text(), 'dd. MMMM yyyy HH:mm:ss'),
+					description: $(this).find('description').text()
+				});
+				feedItemCollection.push(feedItemModel, []);
+		  });
+			feedView.addFeedItemCollection(feedItemCollection);
+		});
  		this.displayOnly("feed");	
   },
   setMaptyp: function(typ) {

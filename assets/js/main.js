e1432d01398a93b82786bf0a5ad28c0851e82743
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2012-06-03 17:43:19 +0200

    worked on offline behaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,18 +9,26 @@ $(document).ready(function(){
 	}
 	finally{
 	}
-	
 	window.scrollTo(0, 1);
-	$('#map_canvas').height(window.innerHeight - $('#navigation').height());
 });
 
 var supportsOrientationChange = "onorientationchange" in window,
     orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
-    
+
 window.addEventListener(orientationEvent, function() {
-	$('#map_canvas').height(window.innerHeight - $('#navigation').height());
   appRouter.mapView.resizeMap();
 }, false);
 
 var appRouter = new AppRouter();
 
+function logEvent(event) {
+  console.log(event.type);
+}
+
+window.applicationCache.addEventListener('checking',logEvent,false);
+window.applicationCache.addEventListener('noupdate',logEvent,false);
+window.applicationCache.addEventListener('downloading',logEvent,false);
+window.applicationCache.addEventListener('cached',logEvent,false);
+window.applicationCache.addEventListener('updateready',logEvent,false);
+window.applicationCache.addEventListener('obsolete',logEvent,false);
+window.applicationCache.addEventListener('error',logEvent,false);  

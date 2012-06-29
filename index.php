44bf9ec9ae4a6252826c729ee7a7db577f1580e8
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2012-06-29 20:16:20 +0200

    added map navigation button

--- index.php
+++ index.php
@@ -82,15 +82,18 @@ else
         <nav id="header-navigation">
           <ul>
             <?php if ( !$isMobile ) { ?>
+            <li class='menu-item show_map'>
+              <a href='javascript:void(0)' onclick='window.Trinkbrunnen.scrollMap()'>Karte</a>
+            </li>
             <li class='menu-item feed'>
               <a href='javascript:void(0)' onclick='window.Trinkbrunnen.showRssFeed()'>Wasser-News</a>
             </li>
             <?php } ?>
-            <li class='menu-item about'>
-              <a href='javascript:void(0)' onclick='window.Trinkbrunnen.showAbout()'>Impressum</a>
-            </li>
             <li class='menu-item' id='lake_temperature'>
               <a href='http://www.salzburg.at/sbg_aktuell.html?AktuellID=WT' target='_blank'>Seetemperaturen</a>
+            </li>
+            <li class='menu-item about'>
+              <a href='javascript:void(0)' onclick='window.Trinkbrunnen.showAbout()'>Impressum</a>
             </li>
           </ul>
         </nav>
